673bf16bf8aa192613c821425d2921d96cb0a2ab
ilyaonoff <ilyaonof@yandex.ru>
2021-06-14 01:28:50 +0300

    Delete unnecessary files

--- src/main/java/ru/hse/rekoder/repositories/FolderGraphLookupRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.hse.rekoder.repositories;
-
-import ru.hse.rekoder.model.Folder;
-
-import java.util.List;
-
-public interface FolderGraphLookupRepository {
-    List<Integer> getSubTree(int folderId);
-}
