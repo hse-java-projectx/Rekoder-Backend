e2e418fd9e8b5e4fb4921de8e4db7c4d43555039
ilyaonoff <ilyaonof@yandex.ru>
2021-06-14 01:00:22 +0300

    Rewrite JwtConfig

--- src/main/java/ru/hse/rekoder/jwt/JwtConfig.java
+++ src/main/java/ru/hse/rekoder/jwt/JwtConfig.java
@@ -2,6 +2,7 @@ package ru.hse.rekoder.jwt;
 
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,27 +11,17 @@ import javax.crypto.SecretKey;
 
 @Configuration
 @PropertySource("classpath:application.properties")
+@Getter
 public class JwtConfig {
-    @Value("${jwt.encode-secret-key}")
-    private String encodeSecretKey;
-    @Value("${jwt.token-expires-after-days}")
-    private Integer tokenExpirationAfterDays;
-    private SecretKey secretKey;
-    @Value("${jwt.token-prefix}")
-    private String tokenPrefix;
+    private final Integer tokenExpirationAfterDays;
+    private final SecretKey secretKey;
+    private final String tokenPrefix;
 
-    public SecretKey getSecretKey() {
-        if (secretKey == null) {
-            this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(encodeSecretKey));
-        }
-        return secretKey;
-    }
-
-    public int getTokenExpirationAfterDays() {
-        return tokenExpirationAfterDays;
-    }
-
-    public String getTokenPrefix() {
-        return tokenPrefix;
+    public JwtConfig(@Value("${jwt.encode-secret-key}") String encodeSecretKey,
+                     @Value("${jwt.token-expires-after-days}") Integer tokenExpirationAfterDays,
+                     @Value("${jwt.token-prefix}") String tokenPrefix) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(encodeSecretKey));
+        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+        this.tokenPrefix = tokenPrefix;
     }
 }
